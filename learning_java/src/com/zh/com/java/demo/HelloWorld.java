900474a4c3735cc445a77beff29c9e2d774d160f
zhaoshouxuan <zhaoshouxuan@aerozhonghuan.com>
2019-07-13 16:33:25 +0800

    demo_java

---
+++ learning_java/src/com/zh/com/java/demo/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.zh.com.java.demo;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello java");
+    }
+}
